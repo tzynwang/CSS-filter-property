1aaa93db1cdf1a18b5c465e00f479a5043f1822e
tzyn <yukarinerin@gmail.com>
2021-03-07 20:10:46 +0800

    remove credit.html and credit.js

--- js/credit.js
+++
@@ -1,6 +0,0 @@
-// dark mode
-let dark = document.getElementById("dark");
-dark.addEventListener("click", function () {
-  document.body.classList.toggle("dark");
-  document.getElementById("footer").classList.toggle("dark-footer");
-})
